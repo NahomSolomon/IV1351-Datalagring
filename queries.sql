4204eb89c309cff4aec15bd356199850205f2cd2
Nahom <nahomkifle6@gmail.com>
2023-08-20 21:25:09 +0200

    update

--- queries.sql
+++ queries.sql
@@ -1,19 +1,6 @@
---Shows number of lessons in each month of the given year
-
-CREATE VIEW lesson_counts_per_month AS
-SELECT DATE_TRUNC('month', date_and_time::timestamp) AS month, COUNT(*) AS num_lessons
-FROM public.lesson
-WHERE DATE_PART('year', date_and_time::timestamp) = 2023
-GROUP BY month
-ORDER BY month;
-
-
-
 --Show the number of lessons given per month during a specified year.
--- This query is expected to be performed a few times per week.
 --It shall be possible to retrieve the total number of lessons per month (just one number per month) 
 --and the specific number of individual lessons, group lessons and ensembles (three numbers per month). 
-
 CREATE VIEW lesson_count_month AS
 SELECT 
    DATE_TRUNC('month', date_and_time::timestamp) AS month,
@@ -32,9 +19,13 @@ GROUP BY
     month
 ORDER BY 
     month;
+-- It is selecting and grouping the data in month, number of lesson and the three types of lessons as well.
+--It is taking the date from the table lesson LEFT join with the other types of lessons. Then we narrow down the result to the specified year and grouping them in the respective month. 
+--Lastly it will order the result by the month column
+
+
 
 --Show how many students there are with no sibling, with one sibling, with two siblings
-
 CREATE VIEW count_sibling AS
 SELECT 
     COUNT(CASE WHEN sibling_count = 0 THEN 1 ELSE NULL END) AS num_no_sibling,
@@ -47,16 +38,20 @@ FROM (
     LEFT JOIN public.sibling sb ON s.student_id = sb.student_id
     GROUP BY s.student_id
 ) AS sibling_counts;
-
+--It will select and group the data in no_siblings, one_sibling and two_siblings.
+--It will take the data from the student, that is student_id and left join with sibling table as well as counting the siblings of each student.
+--It is then filtered by number of siblings.
 
 
 --Shows insctructors and the number of lessons they give
-
 CREATE VIEW instructor_lessons AS
 SELECT instructor_id, COUNT(lesson.lesson_id) AS total_lessons
 FROM lesson
 INNER JOIN instructor ON lesson.instructor_id = instructor.id
 GROUP BY instructor_id
 ORDER BY total_lessons DESC;
+--It will select and group into instructor_id and total number of lessons. 
+--It takes the data from the lesson table and Inner joins, matching the instructor_id and the instructor_id column in table lesson. 
+--Then it groups them to each instructor and finally order the result in descending number of lessons.
 
 
